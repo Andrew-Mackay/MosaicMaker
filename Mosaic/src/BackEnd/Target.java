b5ed9e706b309abe2026843fb90a456eeba93c55
Chimp022 <mackay-andrew@hotmail.com>
2016-12-28 21:02:30 +0000

    Calculated required number of pictures

--- Mosaic/src/BackEnd/Target.java
+++ Mosaic/src/BackEnd/Target.java
@@ -10,21 +10,50 @@ import javax.imageio.ImageIO;
 public class Target {
 	
 	private BufferedImage img;
-	private double width, height, sourceWidth, sourceHeight;
+	private int width, height, sourceDiagonal;
 	private double targetRatio, sourceRatio;
+	private int targ;
+	private int count;
 	
 	public Target(int count, Path address, List<Picture> pictures) throws IOException{
+		this.count = count;
 		img = ImageIO.read(address.toFile());
 		height = img.getHeight();
 		width = img.getWidth();
-		sourceHeight = pictures.get(0).getHeight();
-		sourceWidth = pictures.get(0).getWidth();
+		sourceDiagonal = pictures.get(0).getDiagonal();
 		//System.out.println(height + " " + width);
-		targetRatio = width/height;
-		sourceRatio = sourceWidth/sourceHeight;
-		System.out.println(targetRatio + " " + sourceRatio);
+		//targ = width*height;
+		//sourceRatio = sourceDiagonal/sourceDiagonal;
+		//System.out.println(targetRatio + " " + sourceRatio);
+		computeRects();
 		
 	}
+	
+	private void computeRects(){
+		int gcd = gcd(height, width);
+		targ = (width/gcd) * (height/gcd);
+		int numPics = (count/targ)*targ;
+		System.out.println(gcd);
+		System.out.println(count);
+		System.out.println(numPics);
+	}
+	
+
+    static int gcd(int x, int y)
+    {
+        int r=0, a, b;
+        a = (x > y) ? x : y; // a is greater number
+        b = (x < y) ? x : y; // b is smaller number
+ 
+        r = b;
+        while(a % b != 0)
+        {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return r;
+    }
 }
 /*29 pictures 3200:2400
 final 3200:2400 1.333
